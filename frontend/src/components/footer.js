12f36475a420c3191125e852176032beb7d12cc5
satyam8254 <satyamraj8254@gmail.com>
2022-02-12 01:43:24 +0530

    first commit

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,9 +1,47 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {} from "react-bootstrap";
+import "./footer.css"
+import Facebook from '../images/facebook.svg';
+import Instagram from '../images/instagram.svg';
+import Linkedin from '../images/linkedin.svg';
+
 function footer(){
     return(
-        <div>
+        <div className='footer-container'>
+            <div>
+          <h6>ONLINE SHOPPING</h6>
+          <ul>
+            <li>MEN</li>
+            <li>WOMEN</li>
+          </ul>
+      </div>
+      <div>
+          <h6>USEFUL LINKS</h6>
+          <ul>
+              <li>FAQ</li>
+              <li>T&C</li>
+          </ul>
+      </div>
+      <div>
+          <h6>APP ON MOBILE</h6>
+      </div>
+      <div>
+          <h6>CAREER</h6>
+          <ul>
+              <li>Blogs</li>
+              <li>Create</li>
+          </ul>
+      </div>
+      <div>
+          <h6>CONTACT</h6>
+      </div>
+      <div>
+          <h6>KEEP IN TOUCH</h6>
+          <div className='social-icons'>
+              <img src={Facebook} alt="" />
+              <img src={Instagram} alt="" />
+              <img src={Linkedin} alt="" />
+          </div>
+      </div>
 
         </div>
 
--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Bodyoffer from "./bodyoffer1";
 import Carousel from "./carousel";
 import Header from "./header";
+import Footer from "./footer";
 function home(){
     return(
         <div>
             <Header/>
             <Carousel/>
             <Bodyoffer/>
+            <Footer/>    
         </div>
     );
 }
